52c5899ab347cc3db5e25effcc506d3a8a001096
Carlos Silva <silvacj90@gmail.com>
2023-11-03 21:52:29 -0400

    properly display exp until upgrade

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const exp = document.querySelector("#exp");
 const rank = document.querySelector("#rank");
 const actionPanel = document.querySelector("#actions");
 const counterDiv = document.querySelector("#counter");
+const unlockRequirement = document.querySelector("#unlock-requirement");
+
 const counterArr = [];
 
 const player = {
   experience: 0,
-  rank: "student",
+  rank: "Student",
+  requirement: 0,
 };
 
 const gamePath = {
@@ -17,6 +20,9 @@ const gamePath = {
     active: false,
     passive: false,
     id: 0,
+    rank: "Student",
+    level: 0,
+    nextRequirement: 1000,
   },
   1000: {
     name: "Taijutsu Lessons",
@@ -26,23 +32,45 @@ const gamePath = {
     active: false,
     passive: true,
     id: 1,
+    rank: "Student",
+    level: 0,
+    nextRequirement: 10000,
   },
   10000: {
     name: "Konoha Academy",
     cost: 10000,
-    reward: 500,
+    reward: 1000,
     multiplier: 1.2,
     active: false,
     passive: true,
     id: 2,
+    rank: "Student",
+    level: 0,
+    nextRequirement: 20000,
   },
+  20000: {
+    name: "D Rank Mission",
+    cost: 20000,
+    reward: 2000,
+    multiplier: 1.3,
+    active: false,
+    passive: true,
+    id: 3,
+    rank: "Genin",
+    level: 0,
+    nextRequirement: 50000,
+  },
+};
+
+const upgradeLevel = (program) => {
+  program.level++;
 };
 
 const compareRewards = (program) => {
   if (program.passive && player.experience >= program.cost) {
     player.experience -= program.cost;
-    program.cost += 100;
-    program.multiplier += 0.2;
+    program.cost = program.cost * program.multiplier;
+    program.reward = program.reward * program.multiplier;
     displayCost(program);
     render();
 
@@ -55,6 +83,7 @@ const compareRewards = (program) => {
     }
   } else {
     player.experience += program.reward;
+    player.requirement = program.nextRequirement;
 
     render();
   }
@@ -69,11 +98,15 @@ const displayCost = (program) => {
   }
 
   if (program.passive) {
-    newStat.innerText = `+${Math.round(
+    upgradeLevel(program);
+    newStat.innerText = `Level ${program.level}: +${Math.round(
       program.reward * program.multiplier
-    )}s cost: ${program.cost}`;
+    )} exp per second / Cost to upgrade: ${Math.round(program.cost)}`;
   } else {
-    newStat.innerText = `+${Math.round(program.reward)}s cost: ${program.cost}`;
+    upgradeLevel(program);
+    newStat.innerText = `Level ${program.level}: +${Math.round(
+      program.reward
+    )} exp per click / Cost to use: ${Math.round(program.cost)}`;
   }
 };
 
@@ -95,14 +128,18 @@ const unlockProgram = () => {
     const program = gamePath[key];
     if (!program.active && player.experience >= program.cost) {
       generateProgram(program);
+      player.rank = program.rank;
+      player.requirement = program.nextRequirement;
+      unlockRequirement.innerText = player.requirement;
       program.active = true;
     }
   }
 };
 
 const render = () => {
-  exp.innerText = player.experience;
+  exp.innerText = Math.round(player.experience);
   rank.innerText = player.rank;
+
   unlockProgram();
 };
 
